a3a20be08eb2cca3232081978787bee16ae702f9
adam080880 <muhamadadam20@gmail.com>
2020-07-14 16:31:20 +0700

    create design mvc design pattern

---
+++ src/configs/database.js
@@ -0,0 +1,16 @@
+const {
+  DB_HOST: host,
+  DB_USER: user,
+  DB_PASS: password,
+  DB_NAME: database,
+} = process.env;
+const mysql = require("mysql");
+
+const con = mysql.createConnection({
+  host,
+  user,
+  password,
+  database,
+});
+
+module.exports = con;
