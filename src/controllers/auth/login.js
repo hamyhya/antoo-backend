f8ee6fcfd5af398067964f1592bd9497288ddb61
adam080880 <muhamadadam20@gmail.com>
2020-07-15 10:52:00 +0700

    fix module not found

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,28 +1,26 @@
-require('dotenv').config()
-const jwt = require('jsonwebtoken')
-const response = require('../utils/response')
-const authValidator = require("../../validators/auth")
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const response = require("../../utils/response");
+const authValidator = require("../../validators/auth");
 
-const { APP_KEY } = process.env
+const { APP_KEY } = process.env;
 
 module.exports = (req, res) => {
   const loginValidator = authValidator.login(req.body);
   if (loginValidator.status) {
-    const {
-      id,
-      email,
-      password
-    } = loginValidator.passed
+    const { id, email, password } = loginValidator.passed;
     try {
       const loggedInUser = {
-        id, email, password,
-        token: jwt.sign(req.body, APP_KEY)
-      }
-      res.status(200).send(response(true, loginValidator.msg, loggedInUser))
+        id,
+        email,
+        password,
+        token: jwt.sign(req.body, APP_KEY),
+      };
+      res.status(200).send(response(true, loginValidator.msg, loggedInUser));
     } catch (e) {
       res.status(500).send(response(false, e.message));
     }
   } else {
-    res.status(400).send(response(false, loginValidator.msg))
+    res.status(400).send(response(false, loginValidator.msg));
   }
-}
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const register = require("../controllers/auth/register");
 const activation = require("../controllers/auth/activation");
-const login = require("../controller/auth/login");
+const login = require("../controllers/auth/login");
 
 router.post("/register", register);
 router.patch("/activation", activation);
