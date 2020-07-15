c75c6d49be090b9065f96f2c54c4b2b109c85f6a
adam080880 <muhamadadam20@gmail.com>
2020-07-15 20:21:48 +0700

    fix bugs in top up validator

--- src/validators/transaction.js
+++ src/validators/transaction.js
@@ -5,10 +5,18 @@ module.exports = {
   topup: (request) => {
     const { amount, user_id } = request;
     if (!validator.isEmpty(amount) && !validator.isEmpty(user_id)) {
-      return throwValidator(true, "Success", {
-        amount: validator.toInt(amount),
-        user_id: validator.toInt(user_id),
-      });
+      if (Number.isInteger(parseInt(amount))) {
+        if (amount > 0) {
+          return throwValidator(true, "Success", {
+            amount: validator.toInt(amount),
+            user_id: validator.toInt(user_id),
+          });
+        } else {
+          return throwValidator(false, "Amount is more than 0", request);
+        }
+      } else {
+        return throwValidator(false, "Amount must be integer", request);
+      }
     } else {
       return throwValidator(
         false,
