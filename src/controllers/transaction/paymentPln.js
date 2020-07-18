ca0eff0e1d74cda7e505a348e47245402606effe
adam080880 <muhamadadam20@gmail.com>
2020-07-18 09:31:06 +0700

    fix pln payment

--- src/controllers/transaction/paymentPln.js
+++ src/controllers/transaction/paymentPln.js
@@ -6,7 +6,7 @@ const plnGenerator = require("../../utils/plnGenerator");
 const response = require("../../utils/response");
 
 module.exports = async (req, res) => {
-  const plnGeneratorProccess = await plnGenerator(24);
+  const plnGeneratorProccess = await plnGenerator(25);
   const paymentPlnValidatorChecked = await paymentPlnValidator({
     ...req.body,
     ...{ user_id: req.me.id, tokenPln: plnGeneratorProccess },
@@ -19,11 +19,13 @@ module.exports = async (req, res) => {
       );
       res.status(200).send(response(true, "Success", consumePlnPaymentModel));
     } catch (e) {
+      delete paymentPlnValidatorChecked.passed.tokenPln;
       res
         .status(500)
         .send(response(false, e.message, paymentPlnValidatorChecked.passed));
     }
   } else {
+    delete paymentPlnValidatorChecked.passed.tokenPln;
     res
       .status(400)
       .send(
